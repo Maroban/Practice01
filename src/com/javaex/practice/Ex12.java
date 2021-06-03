00dbb1a2a957dae681d75a106827a8afe558c360
Maroban <dbtmdqja207@naver.com>
2021-06-03 16:25:45 +0900

    6/03 수정

--- src/com/javaex/practice/Ex12.java
+++ src/com/javaex/practice/Ex12.java
@@ -6,12 +6,13 @@ public class Ex12 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("반지름을 입력하세요: ");
 		double r = sc.nextDouble();
-		System.out.println("원의 넓이는 " + (r*r*3.14));
-		sc.close();
 
+		System.out.println("원의 넓이는 " + (r * r * 3.14));
+
+		sc.close();
 	}
 
 }
